c5626d9822db50ed051b5d2bc7ac4b23b07d4e3f
MandisLoh <mandisloh13@gmail.com>
2022-07-25 20:33:15 +0800

    Revert "gunicorn config"
    This reverts commit c5ea3d539e91815731fb9d36ee4dde82c59ed4e2.

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\mandi\Dropbox\My PC (LAPTOP-RU8NIR0H)\Desktop\Term 5\60.005 HCI & AI\flask_set_up\.venv
+set VIRTUAL_ENV=C:\Users\mandi\github-classroom\flask_set_up\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
